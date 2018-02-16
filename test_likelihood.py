e0e70d92d022b8e1b2c6322351b40444ef86379a
Peter Wilton <peterrwilton@gmail.com>
2018-02-15 17:18:32 -0800

    some updates to test_likelihood

--- test_likelihood.py
+++ test_likelihood.py
@@ -1,11 +1,12 @@
 import deepdish as dd
 import numpy as np
+import numpy.random as npr
 import afd
-from likelihood import calc_likelihood
+from likelihood import calc_likelihood, single_locus_log_likelihood
 
 N = 1000
 ab = 0.3
-ppoly = 1
+ppoly = 0.1
 breaks = afd.get_breaks_symmetric(N = N, uniform_weight = 0.2, min_bin_size = 0.01)
 freqs = afd.get_binned_frequencies(N, breaks)
 lf = np.log(freqs)
@@ -14,7 +15,8 @@ l1mf = np.log(1-freqs)
 
 cm, lo, all_majorminor = dd.io.load('empirical_onecm.h5')
 
-key = lo['chrM'].keys()[0]
+bam_fns = lo['chrM'].keys()
+
 lo_sorted = {}
 for chrom, lochrom in lo.iteritems():
     lo_sorted[chrom] = {}
@@ -44,8 +46,10 @@ import time; time.time()
 
 nbetas = len(regkeys)*3*rowlen
 betas = np.zeros(nbetas)
+#npr.seed(0); betas = npr.uniform(-0.1,0.1, size=nbetas)
 pars = np.concatenate((betas, (ab, ppoly)))
 
+'''
 import schwimmbad
 
 pool = schwimmbad.MultiPool(4)
@@ -57,3 +61,12 @@ print calc_likelihood(pars, cm, lo, all_majorminor, blims, rowlen, freqs, breaks
 dur = time.time() - t
 print 'took {} seconds'.format(dur)
 pool.close()
+'''
+
+if __name__ == '__main__':
+    bam = bam_fns[0]
+    for i in range(600):
+        print (i, single_locus_log_likelihood(pars, 'chrM', bam, i, cm, lo, all_majorminor, blims, rowlen, freqs, breaks, lf, l1mf, regkeys))
+
+    print freqs
+    print bam
