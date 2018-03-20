9b82aa021ee7b60c4a208ae7c207a877ee24e960
Peter Wilton <peterrwilton@gmail.com>
2018-03-20 13:30:53 -0700

    use folded beta on [0,1] instead of beta on [0,0.5]

--- beta_with_spikes_integrated.py
+++ beta_with_spikes_integrated.py
@@ -38,8 +38,10 @@ def get_lpf(params, x, window_boundaries = None):
     A = np.exp(lA)
     B = np.exp(lB)
     z = logistic(expitz)
-    If = betainc(A,B, 2*v)
-    lpf = np.concatenate(((np.log(z),), np.log(np.diff(If)*(1-z))))
+    If_l = betainc(A,B, v)
+    If_h = betainc(A,B, 1-v)
+    pf = (np.diff(If_l) + np.diff(If_h[::-1])[::-1])*(1-z)
+    lpf = np.concatenate(((np.log(z),), np.log(pf)))
     return lpf
 
 def get_gradient(params, x, window_boundaries = None, eps = 1e-7):
@@ -50,34 +52,44 @@ def get_gradient(params, x, window_boundaries = None, eps = 1e-7):
     A = np.exp(lA)
     B = np.exp(lB)
     z = logistic(expitz)
-    If = betainc(A,B, 2*v)
-    pf = np.diff(If)*(1-z)
+    If_l = betainc(A,B, v)
+    If_h = betainc(A,B, 1-v)
+    pf = (1-z)*(np.diff(If_l) + np.diff(If_h[::-1])[::-1])
     lpf = np.log(pf)
 
     # A
     Ape = np.exp(lA+eps)
-    If_Ape = betainc(Ape,B,2*v)
-    pf_Ape = np.diff(If_Ape)*(1-z)
+    If_Ape_l = betainc(Ape,B,v)
+    If_Ape_h = betainc(Ape,B,1-v)
+    pf_Ape = (1-z)*(np.diff(If_Ape_l) + np.diff(If_Ape_h[::-1])[::-1])
     lpf_Ape = np.log(pf_Ape)
     Dlpf_Ape = (lpf_Ape-lpf)/eps
+    
     Ame = np.exp(lA-eps)
-    If_Ame = betainc(Ame,B,2*v)
-    pf_Ame = np.diff(If_Ame)*(1-z)
+    If_Ame_l = betainc(Ame,B,v)
+    If_Ame_h = betainc(Ame,B,1-v)
+    pf_Ame = (1-z)*(np.diff(If_Ame_l) + np.diff(If_Ame_h[::-1])[::-1])
     lpf_Ame = np.log(pf_Ame)
     Dlpf_Ame = (lpf-lpf_Ame)/eps
+    
     Dlpf_A = (Dlpf_Ape+Dlpf_Ame)/2.0
+    
     # B
     Bpe = np.exp(lB+eps)
-    If_Bpe = betainc(A,Bpe,2*v)
-    pf_Bpe = np.diff(If_Bpe)*(1-z)
+    If_Bpe_l = betainc(A,Bpe,v)
+    If_Bpe_h = betainc(A,Bpe,1-v)
+    pf_Bpe = (1-z)*(np.diff(If_Bpe_l) + np.diff(If_Bpe_h[::-1])[::-1])
     lpf_Bpe = np.log(pf_Bpe)
     Dlpf_Bpe = (lpf_Bpe-lpf)/eps
+    
     Bme = np.exp(lB-eps)
-    If_Bme = betainc(A,Bme,2*v)
-    pf_Bme = np.diff(If_Bme)*(1-z)
+    If_Bme_l = betainc(A,Bme,v)
+    If_Bme_h = betainc(A,Bme,1-v)
+    pf_Bme = (1-z)*(np.diff(If_Bme_l) + np.diff(If_Bme_h[::-1])[::-1])
     lpf_Bme = np.log(pf_Bme)
     Dlpf_Bme = (lpf-lpf_Bme)/eps
     Dlpf_B = (Dlpf_Bpe+Dlpf_Bme)/2.0
+    
     # z
     zpe = logistic(expitz+eps)
     lpf_zpe = np.log(pf*(1-zpe))
