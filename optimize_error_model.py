b858adebd2205d7f3aa87b151c2b8165915e7345
Peter Wilton <peterrwilton@gmail.com>
2018-12-06 17:08:17 -0800

    add checkpointing, prog-bar, and more summaries

--- optimize_error_model.py
+++ optimize_error_model.py
@@ -10,7 +10,6 @@ import numpy.random as npr
 import tensorflow as tf
 import tables
 from scipy.special import logsumexp
-#import tensorflow.train
 
 import beta_with_spikes_integrated as bws
 import errormodel
@@ -33,8 +32,16 @@ parser.add_argument('--batch-size', type=int, default=100,
                     help='number of base-pair positions to process for each '
                          'batch')
 parser.add_argument('--learning-rate', type=float, default=0.01)
-#parser.add_argument('--do-not-remove-nonvariable', action='store_true')
+parser.add_argument('--checkpoint-prefix', type=str,
+                    help='save model parameters to this file prefix')
+parser.add_argument('--checkpoint-interval', type=int, default=100,
+                    help='interval of batches between parameter checkpointing')
+parser.add_argument('--restore-prefix', type=str,
+                    help='restore from tensorflow checkpoint'
+                         ' (must have same --hidden-layer-sizes and data)')
 args = parser.parse_args()
+
+do_checkpoint = (args.checkpoint_prefix is not None)
 
 num_pf_params = 3
 
@@ -60,6 +67,9 @@ sess = tf.Session()
 opt = tf.train.RMSPropOptimizer(args.learning_rate)
 global_step = tf.train.get_or_create_global_step()
 
+freqs = bws.get_freqs(args.num_freqs)
+windows = bws.get_freqs(args.num_freqs)
+
 try:
     dat = tables.File(args.data)
     err_mod = errormodel.ErrorModel(dat, args.hidden_layer_sizes,
@@ -74,17 +84,30 @@ try:
         pf_param_0_tf = tf.summary.scalar('pfpar0', params_tf[0])
         pf_param_1_tf = tf.summary.scalar('pfpar1', params_tf[1])
         pf_param_2_tf = tf.summary.scalar('pfpar2', params_tf[2])
+        probnon0_tf = tf.placeholder(shape=(), dtype=tf.float32)
+        probnon0_summary = tf.summary.scalar('probnonzero', probnon0_tf)
     with tf.name_scope('pf_par_grads'):
         pf_param_0_grad_tf = tf.summary.scalar('pfpar0grad', grads_tf[0])
         pf_param_1_grad_tf = tf.summary.scalar('pfpar1grad', grads_tf[1])
         pf_param_2_grad_tf = tf.summary.scalar('pfpar2grad', grads_tf[2])
-
     with tf.name_scope('ll'):
-        tot_ll_tf = tf.placeholder(shape = (), dtype=tf.float32)
+        tot_ll_tf = tf.placeholder(shape=(), dtype=tf.float32)
         tot_ll_tf_summary = tf.summary.scalar('tot_ll', tot_ll_tf)
+    with tf.name_scope('param_moments'):
+        nn_params_tf = params_tf[num_pf_params:]
+        param_mean_tf = tf.reduce_mean(nn_params_tf)
+        mean_summary = tf.summary.scalar('m1', param_mean_tf)
+        param_m2_tf = tf.reduce_mean((nn_params_tf - param_mean_tf)**2)
+        var_summary = tf.summary.scalar('m2', param_m2_tf)
+        param_m3_tf = tf.reduce_mean((nn_params_tf - param_mean_tf)**3)
+        m3_summary = tf.summary.scalar('m3', param_m3_tf)
+        param_m4_tf = tf.reduce_mean((nn_params_tf - param_mean_tf)**4)
+        m4_summary = tf.summary.scalar('m4', param_m4_tf)
+
 
     with tf.name_scope('saver'):
         saver = tf.train.Saver()
+
 
     summaries_tf = tf.summary.merge_all()
 
@@ -99,7 +122,14 @@ try:
         this_rundir = 'summaries/run{:03d}'.format(this_run)
         writer = tf.summary.FileWriter(this_rundir, sess.graph)
 
+    with tf.name_scope('progress_bar'):
+        progbar = tf.keras.utils.Progbar(args.numepochs
+                                         * err_mod.metadata_np.shape[0])
+
     sess.run(tf.global_variables_initializer())
+
+    if args.restore_prefix is not None:
+        saver.restore(sess, args.restore_prefix)
 
 
     global_batch_idx = 0
@@ -112,6 +142,7 @@ try:
         next_data = it.get_next()
         sess.run(it.initializer)
         while True:
+            progbar.update(global_batch_idx)
             try:
                 batch_bams, batch_refs, batch_positions = sess.run(next_data)
             except tf.errors.OutOfRangeError:
@@ -130,16 +161,18 @@ try:
             ll_per_locus = tot_ll / args.batch_size
             dur = time.time()-start
             sess.run(apply_grads, feed_dict={grads_tf:grads_maximize})
-            summ = sess.run(summaries_tf, feed_dict={grads_tf:grads_maximize,
-                                              tot_ll_tf:ll_per_locus})
+
+            pf_params = sess.run(params_tf[:num_pf_params])
+            probnonzero = 1 - np.exp(bws.get_lpf(pf_params, freqs, windows)[0])
+            feed_dict = {grads_tf:grads_maximize, tot_ll_tf:ll_per_locus,
+                         probnon0_tf:probnonzero}
+            summ = sess.run(summaries_tf, feed_dict=feed_dict)
             writer.add_summary(summ, global_batch_idx)
-            print('batch {} params:'.format(global_batch_idx), sess.run(params_tf))
-            print('batch {} grads:'.format(global_batch_idx), grads_maximize)
-            print()
 
             global_batch_idx += 1
-            if global_batch_idx % 100 == 0:
-                saver.save(sess, './model_checkout.ckpt')
+            if do_checkpoint and (global_batch_idx % args.checkpoint_interval
+                                  == 0):
+                saver.save(sess, './'+args.checkpoint_prefix)
 
 finally:
     dat.close()
